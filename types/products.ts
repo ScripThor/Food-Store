8db08110b30cec1b5680d0b8cbb39aede0b501d0
Skjeld <a.dreake@yandex.ru>
2025-02-08 20:46:50 +0300

    Добавление типов для продуктов

---
+++ types/products.ts
@@ -0,0 +1,10 @@
+export interface Product {
+    category_id: number
+    category_slug: string
+    description: string
+    slug: string
+    id: number
+    image: string
+    name: string
+    price: string
+}
