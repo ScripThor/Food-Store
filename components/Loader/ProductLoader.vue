a3d4b35116e393a127eacc1cdb28cba375cf0b01
Skjeld <a.dreake@yandex.ru>
2025-02-13 01:51:09 +0300

    Лоадер для товаров

---
+++ components/Loader/ProductLoader.vue
@@ -0,0 +1,76 @@
+<script setup lang="ts">
+import AddToCartButton from '~/components/Button/AddToCartButton.vue';
+</script>
+
+<template>
+  <div class="product-page container">
+    <div class="product">
+      <div class="product__image"></div>
+      <div class="product__content">
+        <div class="product__content-title">
+          <h1></h1>
+          <span></span>
+        </div>
+        <div class="product__content-description"></div>
+        <div class="product__content-footer"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.product-page {
+  margin-top: 50px;
+}
+
+.product {
+  display: flex;
+  gap: 20px;
+
+  &__image {
+    width: 600px;
+    height: 400px;
+    background: #e7e7e7;
+    border-radius: 25px;
+  }
+
+  &__content {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    padding: 25px 0;
+
+    &-title {
+      & h1 {
+        width: 400px;
+        height: 40px;
+        border-radius: 25px;
+        background: #e7e7e7;
+      }
+
+      & span {
+        width: 200px;
+        height: 40px;
+        display: block;
+        border-radius: 25px;
+        background: #e7e7e7;
+      }
+    }
+
+    &-description {
+      width: 500px;
+      height: 150px;
+      display: block;
+      border-radius: 25px;
+      background: #e7e7e7;
+    }
+
+    &-footer {
+      width: 500px;
+      height: 40px;
+      border-radius: 25px;
+      background: #e7e7e7;
+    }
+  }
+}
+</style>
--- pages/product/[product]/index.vue
+++ pages/product/[product]/index.vue
@@ -4,51 +4,50 @@ import { useProductsStore } from '@/stores/useProductsStore';
 import type { Product } from '@/types/products';
 
 import AddToCartButton from '@/components/Button/AddToCartButton.vue';
+import ProductLoader from '~/components/Loader/ProductLoader.vue';
 
 const route = useRoute();
 const productStore = useProductsStore();
 
 const slug = route.params.product as string;
 const product = ref<Product | null>(null);
-const isLoading = ref(true); // Состояние загрузки
+const isLoading = ref(true);
 
 onMounted(async () => {
   await productStore.loadProducts();
   product.value = productStore.products.find((p) => p.slug === slug) || null;
-  isLoading.value = false; // Загрузка завершена
+  isLoading.value = false;
 });
 </script>
 <template>
-  <template>
-    <div v-if="isLoading">Загрузка...</div>
-    <div v-else-if="product" class="product-page container">
-      <div class="product">
-        <div class="product__image">
-          <img :src="product.image" :alt="product.name" />
+  <product-loader v-if="isLoading" />
+  <div v-else-if="product" class="product-page container">
+    <div class="product">
+      <div class="product__image">
+        <img :src="product.image" :alt="product.name" />
+      </div>
+      <div class="product__content">
+        <div class="product__content-title">
+          <h1>{{ product.name }}</h1>
+          <span>{{ product.weight }} г.</span>
         </div>
-        <div class="product__content">
-          <div class="product__content-title">
-            <h1>{{ product.name }}</h1>
-            <span>{{ product.weight }} г.</span>
-          </div>
-          <div class="product__content-description">
-            {{ product.description }}
-          </div>
-          <div class="product__content-composition">
-            <span>Состав</span>
-            <p>{{ product.composition }}</p>
-          </div>
-          <div class="product__content-footer">
-            <add-to-cart-button />
-            <span>{{ product.price }} руб.</span>
-          </div>
+        <div class="product__content-description">
+          {{ product.description }}
+        </div>
+        <div class="product__content-composition">
+          <span>Состав</span>
+          <p>{{ product.composition }}</p>
+        </div>
+        <div class="product__content-footer">
+          <add-to-cart-button />
+          <span>{{ product.price }} руб.</span>
         </div>
       </div>
     </div>
-    <div v-else>
-      <p>Товар не найден.</p>
-    </div>
-  </template>
+  </div>
+  <div v-else>
+    <p>Товар не найден.</p>
+  </div>
 </template>
 
 <style scoped lang="scss">
@@ -58,8 +57,12 @@ onMounted(async () => {
 
 .product {
   display: flex;
+  gap: 20px;
 
   &__image {
+    & img {
+      border-radius: 20px;
+    }
   }
 
   &__content {
