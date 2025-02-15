c2adb5f1dc0bdbf7e4a8d76f1222003b76928e98
Skjeld <a.dreake@yandex.ru>
2025-02-15 23:08:16 +0300

    Каунтер в карточку товара

--- pages/product/[product]/index.vue
+++ pages/product/[product]/index.vue
@@ -5,6 +5,8 @@ import type { Product } from '@/types/products';
 
 import AddToCartButton from '@/components/Button/AddToCartButton.vue';
 import ProductLoader from '@/components/Loader/ProductLoader.vue';
+import TheCounter from '@/components/Counter/TheCounter.vue';
+import { useCartStore } from '@/stores/useCartStore';
 
 const route = useRoute();
 const productStore = useProductsStore();
@@ -13,12 +15,54 @@ const slug = route.params.product as string;
 const product = ref<Product | null>(null);
 const isLoading = ref(true);
 
+const cartStore = useCartStore();
+const showCounter = ref(false);
+
 onMounted(async () => {
   await productStore.loadProducts();
-  product.value = productStore.products.find((p) => p.slug === slug) || null;
+  const foundProduct = productStore.products.find((p) => p.slug === slug);
+  if (foundProduct) {
+    product.value = foundProduct;
+    updateCounterState();
+  } else {
+    console.error('Товар не найден');
+  }
   isLoading.value = false;
 });
+
+watch(
+  () => cartStore.items,
+  () => updateCounterState(),
+  { deep: true },
+);
+
+const updateCounterState = () => {
+  if (product.value) {
+    const item = cartStore.items.find((i) => i.id === product.value.id);
+    showCounter.value = !!item && item.quantity > 0;
+  }
+};
+
+const handleAddToCart = () => {
+  if (product.value) {
+    cartStore.addToCart(product.value);
+    showCounter.value = true;
+  }
+};
+
+const increaseQuantity = () => {
+  if (product.value) {
+    cartStore.incrementQuantity(product.value.id);
+  }
+};
+
+const decreaseQuantity = () => {
+  if (product.value) {
+    cartStore.decrementQuantity(product.value.id);
+  }
+};
 </script>
+
 <template>
   <product-loader v-if="isLoading" />
   <div v-else-if="product" class="product-page container">
@@ -39,7 +83,19 @@ onMounted(async () => {
           <p>{{ product.composition }}</p>
         </div>
         <div class="product__content-footer">
-          <add-to-cart-button :product="product" />
+          <the-counter
+            v-if="showCounter"
+            :value="
+              cartStore.items.find((i) => i.id === product.id)?.quantity || 0
+            "
+            @increase="increaseQuantity"
+            @decrease="decreaseQuantity"
+          />
+          <add-to-cart-button
+            v-else
+            :product="product"
+            @click="handleAddToCart"
+          />
           <span>{{ product.price }} руб.</span>
         </div>
       </div>
